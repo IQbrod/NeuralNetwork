b71c10ab2e4fdcd3c97c740fd2336e639f673343
IQbrod <iqbrod@outlook.fr>
2018-05-15 15:48:27 +0200

    Using pytorch

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import numpy as np
+import torch as tor
 
 '''
 Le modèle apprend à reconnaitre à partir de données en entrée et en sortie
 Le modèle apprend jusqu'à une précision K
 '''
+
+#Specification matérielle
+dtype = tor.float
+device = tor.device("cpu")
+
+#Specifications du modèle
 K = 10e-5       #Précision du modèle
 
 N = 64          #Nombre de données
@@ -14,11 +20,11 @@ D_in = 1000     #Dimension Entrée
 H = 100         #Dimension Intermediaire
 D_out = 10      #Dimension Sortie
 
-x = np.random.randn(N, D_in)    #Entrées Aléatoires
-y = np.random.randn(N, D_out)   #Sorties Aléatoires
+x = tor.randn(N, D_in, device=device, dtype=dtype)    #Entrées Aléatoires
+y = tor.randn(N, D_out, device=device, dtype=dtype)   #Sorties Aléatoires
 
-w1 = np.random.randn(D_in, H)   #Poids d'entrée vers inter
-w2 = np.random.randn(H, D_out)  #Poids d'inter vers sortie
+w1 = tor.randn(D_in, H, device=device, dtype=dtype)   #Poids d'entrée vers inter
+w2 = tor.randn(H, D_out, device=device, dtype=dtype)  #Poids d'inter vers sortie
 
 learn_rate = 1e-6
 
